a245ddd3d428174e82ef0c2be994c0a351d98b9a
Will Garside <worgarside@gmail.com>
2022-06-03 12:52:11 +0100

    Add logging to main script

--- src/monitor.py
+++ src/monitor.py
@@ -1,12 +1,38 @@
 """Monitors plant soil moisture and sends it to Home Assistant"""
+from datetime import datetime
 from json import dumps
+from logging import getLogger, DEBUG
 from os import getenv
+from os.path import join
+from pathlib import Path
 from time import sleep
 from paho.mqtt.publish import multiple
 from dotenv import load_dotenv
+from wg_utilities.functions import (  # pylint: disable=no-name-in-module
+    force_mkdir,
+    try_float,
+)
+from wg_utilities.loggers import add_stream_handler, add_file_handler
 
 from plant import Plant, TEST_MODE
 
+
+LOGGER = getLogger(__name__)
+LOGGER.setLevel(DEBUG)
+
+add_file_handler(
+    LOGGER,
+    logfile_path=force_mkdir(
+        join(
+            Path.home(),
+            "logs",
+            "plant-monitor",
+            f"{datetime.today().strftime('%Y-%m-%d')}.log",
+        ),
+        path_is_file=True,
+    ),
+)
+add_stream_handler(LOGGER)
 
 load_dotenv()
 
@@ -21,14 +47,29 @@ MQTT_AUTH_KWARGS = dict(
 PLANTS = [
     Plant(i + 1, *plant)
     for i, plant in enumerate(
-        (("Monstera", 0.8, 7.54), ("Yukka", 3.4, 10.8), ("Succulent", 4, 7))
+        (
+            (
+                "Monstera",
+                try_float(getenv("MONSTERA_WET_POINT"), None),
+                try_float(getenv("MONSTERA_DRY_POINT"), None),
+            ),
+            (
+                "Yukka",
+                try_float(getenv("YUKKA_WET_POINT"), None),
+                try_float(getenv("YUKKA_DRY_POINT"), None),
+            ),
+            (
+                "Succulent",
+                try_float(getenv("SUCCULENT_WET_POINT"), None),
+                try_float(getenv("SUCCULENT_DRY_POINT"), None),
+            ),
+        )
     )
 ]
 
 
 def main():
     """Send Home Assistant readings every 30 seconds"""
-
     sleep(5)  # let the sensor initialise
 
     while True:
@@ -44,7 +85,7 @@ def main():
                 msgs,
                 **MQTT_AUTH_KWARGS,
             )
-        print(dumps(msgs, indent=4))
+        LOGGER.debug(dumps(msgs))
         sleep(30)
 
 
