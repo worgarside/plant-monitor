adff8d91cc2118d5326a08cac7c94b0ae834eadb
worgarside <worgarside@gmail.com>
2022-08-04 23:09:35 +0000

    🔄 Synced local 'utilities/dep_updater/' with remote 'utilities/dep_updater/'

--- utilities/dep_updater/install.sh
+++ utilities/dep_updater/install.sh
@@ -8,7 +8,7 @@ mkdir -p "$HOME/logs/dep_updater" || :
 #write out current crontab
 crontab -l > tempcron
 #echo new cron into cron file
-echo "0 4 * * * ${bash_path} ${repo_path}/utilities/dep_updater/dep_updater.sh >> \"$HOME/logs/dep_updater/$(date +20\%y-\%m-\%d).log\" 2>&1" >> tempcron
+echo "0 4 * * * ${bash_path} ${repo_path}/utilities/dep_updater/dep_updater.sh >> \"$HOME/logs/dep_updater/\$(date +20\%y-\%m-\%d).log\" 2>&1" >> tempcron
 # install new cron file
 crontab tempcron
 rm tempcron
